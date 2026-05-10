506320da5e263ddfc1325c182d43932b009f05f9
Ilídio Manuel <EldenLeandro13@gmail.com>
2026-05-10 17:01:45 +0100

    rankingrout.routes.ts

--- api/src/routes/ranking.routes.ts
+++ api/src/routes/ranking.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import * as rankingController from '../controllers/ranking.controller.js';
+import * as rankingController from '../controllers/auth.controller.js';
 
 const router = Router();
 
