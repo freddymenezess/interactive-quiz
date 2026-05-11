49734aea82e0404a7ee80d309c215b19886710d9
Mbalahp <mbalakunsunga1@gmail.com>
2026-05-11 03:50:21 +0100

    feat: add auth page (login and register)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import quizImage from '../public/quizapp.png';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthPage from "./pages/AuthPage";
 
 function App() {
   return (
-    <>
-      <img src={quizImage} alt="Quiz App" />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/" element={<div style={{ color: "#1a1a2e", padding: "2rem" }}>Home (em breve)</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
