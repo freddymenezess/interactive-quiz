9b1c5025a6831fe0b126b586e3cb67218ef24839
Freddy Menezes <friedrichprogrammer@gmail.com>
2026-05-15 04:46:08 +0100

    Update auth.controller.ts

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -12,8 +12,8 @@ const passwordRegex =
 
 const COOKIE_OPTIONS = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === 'production',
-  sameSite: 'lax' as const,
+  secure: true,
+  sameSite: 'none' as const,
   maxAge: 2 * 24 * 60 * 60 * 1000,
 };
 
